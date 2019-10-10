08a9d2533ac989418116fff29b604d23e1124470
rahulsharma <aozps8819>
2019-10-10 22:31:20 +0530

    trying to commit

--- app/src/main/java/com/rahul/dagger/MainActivity.java
+++ app/src/main/java/com/rahul/dagger/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CarComponent carComponent = DaggerCarComponent.create();
+
         carComponent.getCar().drive();
 
     }
